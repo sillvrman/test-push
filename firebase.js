bc54499e876cb049bc7433f69031e46d2bcdf0a5
sillvrman <ali0ali533@gmail.com>
2023-09-05 18:38:19 +0330

    aliii

--- firebase.js
+++ firebase.js
@@ -27,8 +27,34 @@ if (typeof navigator !== "undefined") {
         console.log(currentToken);
         onMessage(messaging, (payload) => {
           console.log("aasasasa", payload);
-          toast("aasasasa");
           const { body, icon, title } = payload?.notification;
+          toast.custom((t) => (
+            <div
+              className={`${
+                t.visible ? "animate-enter" : "animate-leave"
+              } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+            >
+              <div className="flex-1 w-0 p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0 pt-0.5">
+                    <img className="h-10 w-10 rounded-full" src={icon} alt="" />
+                  </div>
+                  <div className="ml-3 flex-1">
+                    <p className="text-sm font-medium text-gray-900">{title}</p>
+                    <p className="mt-1 text-sm text-gray-500">{body}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex border-l border-gray-200">
+                <button
+                  onClick={() => toast.dismiss(t.id)}
+                  className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          ));
           self?.registration?.showNotification(title);
 
           new Notification(title || "", {
